95b371d0a1d52117339ecaa408806f1614c6d42f
AbelHo <abel@arl.nus.edu.sg>
2021-07-24 21:03:51 +0800

    feat(navbar): initial commit

--- docs/init.js
+++ docs/init.js
@@ -3,16 +3,32 @@
 url = "https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/"
 item = "epidemic/cases_malaysia.csv"
 
-async function init(){
+async function init_fetch(){
 	let rq = await fetch("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/README.md");
 	let dat = await rq.text();
 
-	let regexp = new RegExp(/\[`([^)]+)`\]\(([^)]+)\): ([^\n]+)/, 'g');
+	let regexp = new RegExp(/\[`([^)]+).csv`\]\(([^)]+)\): ([^\n]+)/, 'g');
 	list = [...dat.matchAll(regexp)]
+	list.forEach( a => {console.log(a[1]);} )
 	return list
 }
 
-list = init();
+function generateTabs(list){
+	let out = ''
+	list.forEach( a => { out+='<li><a href="#'+a[1]+'">'+a[1]+'</a></li>';} )
+	return out
+}
+
+async function init(){
+	let list = await init_fetch();
+	document.getElementById('nav_top').innerHTML='<ul>'+generateTabs(list)+'</ul>'
+}
+
+init();
+
+// list = init();
+// console.log(list)
+// list.forEach( a => {console.log(a[1]);} )
 
 // dat = await init()
 //const regexp = /\[`([^)]+)\`]/;
