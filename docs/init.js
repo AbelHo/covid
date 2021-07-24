98d23df361ff556fd7bc14d4e1e74da2e3fd9e13
AbelHo <abel@arl.nus.edu.sg>
2021-07-24 19:49:58 +0800

    feat(init): stable

--- docs/init.js
+++ docs/init.js
@@ -3,12 +3,22 @@
 url = "https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/"
 item = "epidemic/cases_malaysia.csv"
 
-rq = await fetch("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/README.md");
-const dat = await rq.text();
+async function init(){
+	let rq = await fetch("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/README.md");
+	let dat = await rq.text();
 
+	let regexp = new RegExp(/\[`([^)]+)`\]\(([^)]+)\): ([^\n]+)/, 'g');
+	list = [...dat.matchAll(regexp)]
+	return list
+}
+
+list = init();
+
+// dat = await init()
 //const regexp = /\[`([^)]+)\`]/;
 // const regexp = new RegExp(/\[`([^)]+)`\]/, 'g');
-regexp = new RegExp(/\[`([^)]+)`\]\(([^)]+)\): ([^\n]+)/, 'g');
-list = [...dat.matchAll(regexp)]
+
+// regexp = new RegExp(/\[`([^)]+)`\]\(([^)]+)\): ([^\n]+)/, 'g');
+// list = [...dat.matchAll(regexp)]
 // list.forEach( a => {console.log(a[3]);} )
 
