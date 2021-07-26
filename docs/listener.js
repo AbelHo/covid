df2c26487d4105bbfa9ca08850c9bccccde3b7ab
AbelHo <abel@arl.nus.edu.sg>
2021-07-26 12:38:08 +0800

    feat(plot html): data source

--- docs/listener.js
+++ docs/listener.js
@@ -2,6 +2,7 @@ function change_tab(item, id){
 	csv2plot(...item);
 	document.getElementById(id).parentNode.childNodes.forEach( a => {document.getElementById(a.id).className="";} )
 	document.getElementById(id).className='navbar_active';
+	document.getElementById("source_current").innerHTML=item[0]; document.getElementById("source_current").href=item[0]; document.getElementById("source_current").target="_blank";
 }
 
 function add_eventlistener(id, func, args){
